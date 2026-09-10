56b8096e8d871a4811d24f8d6da3fa03db28562e
rubenmaderas <maderasprieto@gmail.com>
2026-09-10 18:41:07 +0200

    farm boton

--- dist/modernbot.user.js
+++ dist/modernbot.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ModernBot
-// @version      1.0.7
+// @version      1.0.9
 // @description  A modern grepolis bot
 // @match        http://*.grepolis.com/game/*
 // @match        https://*.grepolis.com/game/*
